10835e841e1631f1f730285b2380ce20e71974b9
littlehoi <littlehoihoi@gmail.com>
2016-06-07 14:40:17 +0800

    tankObject

--- firstone/CounterWebApp/src/main/java/com/pru/hk/ap/ws/bean/Tank.java
+++ firstone/CounterWebApp/src/main/java/com/pru/hk/ap/ws/bean/Tank.java
@@ -5,4 +5,44 @@ public class Tank extends User{
 	private int shootPower;
 	private int speed;
 	private Ability specialAbility;
+	public int getShootRange() {
+		return shootRange;
+	}
+	public void setShootRange(int shootRange) {
+		this.shootRange = shootRange;
+	}
+	public int getShootPower() {
+		return shootPower;
+	}
+	public void setShootPower(int shootPower) {
+		this.shootPower = shootPower;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public Ability getSpecialAbility() {
+		return specialAbility;
+	}
+	public void setSpecialAbility(Ability specialAbility) {
+		this.specialAbility = specialAbility;
+	}
+	@Override
+	public String toString() {
+		return "Tank [shootRange=" + shootRange + ", shootPower=" + shootPower
+				+ ", speed=" + speed + ", specialAbility=" + specialAbility
+				+ ", id=" + id + ", name=" + name + ", pass=" + pass
+				+ ", rank=" + rank + ", world=" + world + ", scene=" + scene
+				+ ", getShootRange()=" + getShootRange() + ", getShootPower()="
+				+ getShootPower() + ", getSpeed()=" + getSpeed()
+				+ ", getSpecialAbility()=" + getSpecialAbility() + ", getId()="
+				+ getId() + ", getName()=" + getName() + ", getPass()="
+				+ getPass() + ", getRank()=" + getRank() + ", getWorld()="
+				+ getWorld() + ", getScene()=" + getScene() + ", toString()="
+				+ super.toString() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + "]";
+	}
+	
 }
